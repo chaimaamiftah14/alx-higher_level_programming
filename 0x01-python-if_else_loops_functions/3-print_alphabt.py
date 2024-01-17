a053d94746ac72ad1d27f43318f17820d4ae645e
chaimaa <chaimaamiftah13@gmail.com>
2024-01-17 09:06:51 -0800

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
-#!/usr/bbin/python3
+#!/usr/bin/python3
 for i in range(97, 123):
-    if i in [101, 113]:
-        continue
-    print("{}".format(chr(i)), end='')
+    if i != 113 and i != 101:
+        print("{}".format(chr(i)), end="")
