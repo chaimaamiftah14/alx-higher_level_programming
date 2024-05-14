042807cad6c690a4db47a59a02c501195a6d7a2f
chaimaa <chaimaamiftah13@gmail.com>
2024-05-14 08:33:23 -0700

    python network 1#

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
 import requests
-
+import sys
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    if len(sys.argv) < 2:
+        sys.exit("Usage: {} <URL>".format(sys.argv[0]))
+
+    url = sys.argv[1]
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for 4XX and 5XX status codes
+        print("Body response:")
+        print("\t- type:", type(response.text))
+        print("\t- content:", response.text)
+    except requests.exceptions.HTTPError as err:
+        print("HTTP Error:", err)
+    except requests.exceptions.ConnectionError as err:
+        print("Error Connecting:", err)
+    except requests.exceptions.Timeout as err:
+        print("Timeout Error:", err)
+    except requests.exceptions.RequestException as err:
+        print("Oops! Something went wrong:", err)
